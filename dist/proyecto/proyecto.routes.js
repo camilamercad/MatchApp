45de337e6e2f2639d247a61c90997417924cecb9
Camila Mercado <129223752+camilamercad@users.noreply.github.com>
2025-10-15 21:48:28 -0300

    Delete dist directory

--- dist/proyecto/proyecto.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { ProyectoController } from "./proyecto.controller.js";
-export const proyectoRouter = Router();
-const proyectoController = new ProyectoController();
-proyectoRouter.post("/", proyectoController.Add);
-proyectoRouter.get("/", proyectoController.GetAll);
-proyectoRouter.get("/:id", proyectoController.GetById);
-//# sourceMappingURL=proyecto.routes.js.map
